bc340f71cb8ea9e676e2dd51e34ddbd52586bb06
Matt Boersma <Matt.Boersma@microsoft.com>
2017-12-14 10:59:57 -0700

    [AKS] refactor formatters to use JMESPath

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_format.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_format.py
@@ -5,6 +5,8 @@
 
 from collections import OrderedDict
 
+from jmespath import compile as compile_jmes, Options
+
 
 def aks_list_table_format(results):
     """"Format a list of managed clusters as summary results for display with "-o table"."""
@@ -17,46 +19,25 @@ def aks_show_table_format(result):
 
 
 def _aks_table_format(result):
-    # move some nested properties up to top-level values
-    properties = result.get('properties', {})
-    promoted = ['kubernetesVersion', 'provisioningState', 'fqdn']
-    result.update({k: properties.get(k) for k in promoted})
-
-    columns = ['name', 'location', 'resourceGroup'] + promoted
-    # put results in an ordered dict so the headers are predictable
-    table_row = OrderedDict()
-    for k in columns:
-        table_row[k] = result.get(k)
-    return table_row
+    parsed = compile_jmes("""{
+        name: name,
+        location: location,
+        resourceGroup: resourceGroup,
+        kubernetesVersion: properties.kubernetesVersion,
+        provisioningState: properties.provisioningState,
+        fqdn: properties.fqdn
+    }""")
+    return parsed.search(result, Options(dict_cls=OrderedDict))
 
 
 def aks_get_versions_table_format(result):
     """Format get-versions upgrade results as a summary for display with "-o table"."""
-    properties = result.get('properties', {})
-    master = properties.get('controlPlaneProfile', {})
-    result['masterVersion'] = master.get('kubernetesVersion', 'unknown')
-    master_upgrades = master.get('upgrades', [])
-    result['masterUpgrades'] = ', '.join(master_upgrades) if master_upgrades else 'None available'
-
-    agents = properties.get('agentPoolProfiles', [])
-    versions, upgrades = [], []
-    for agent in agents:
-        version = agent.get('kubernetesVersion', 'unknown')
-        agent_upgrades = agent.get('upgrades', [])
-        upgrade = ', '.join(agent_upgrades) if agent_upgrades else 'None available'
-        name = agent.get('name')
-        if name:  # multiple agent pools, presumably
-            version = "{}: {}".format(name, version)
-            upgrade = "{}: {}".format(name, upgrades)
-        versions.append(version)
-        upgrades.append(upgrade)
-
-    result['nodePoolVersion'] = ', '.join(versions)
-    result['nodePoolUpgrades'] = ', '.join(upgrades)
-
-    columns = ['name', 'resourceGroup', 'masterVersion', 'masterUpgrades', 'nodePoolVersion', 'nodePoolUpgrades']
-    # put results in an ordered dict so the headers are predictable
-    table_row = OrderedDict()
-    for k in columns:
-        table_row[k] = result.get(k)
-    return [table_row]
+    parsed = compile_jmes("""{
+        name: name,
+        resourceGroup: resourceGroup,
+        masterVersion: properties.controlPlaneProfile.kubernetesVersion || `unknown`,
+        masterUpgrades: properties.controlPlaneProfile.upgrades || [`None available`] | sort(@) | join(`, `, @),
+        nodeVersion: properties.agentPoolProfiles[0].kubernetesVersion || `unknown`,
+        nodeUpgrades: properties.agentPoolProfiles[0].upgrades || [`None available`] | sort(@) | join(`, `, @)
+    }""")
+    return parsed.search(result, Options(dict_cls=OrderedDict))
